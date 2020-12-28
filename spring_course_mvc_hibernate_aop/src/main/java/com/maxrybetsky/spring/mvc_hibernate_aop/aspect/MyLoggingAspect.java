f0fc1c61e0e626756b48f93a6d1f49b31e8eaf7b
MaxRybetsky <stsdewar@ya.ru>
2020-12-28 23:26:40 +0300

    Spring MVC + Hibernate + AOP practise (Part 4) Add AOP Logging option

---
+++ spring_course_mvc_hibernate_aop/src/main/java/com/maxrybetsky/spring/mvc_hibernate_aop/aspect/MyLoggingAspect.java
@@ -0,0 +1,23 @@
+package com.maxrybetsky.spring.mvc_hibernate_aop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyLoggingAspect {
+    @Around("execution(* com.maxrybetsky.spring.mvc_hibernate_aop.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice(
+            ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature methodSignature =
+                (MethodSignature) joinPoint.getSignature();
+        String methodName = methodSignature.getName();
+        System.out.println("Begin of " + methodName);
+        Object targetMethodResult = joinPoint.proceed();
+        System.out.println("End of " + methodName);
+        return targetMethodResult;
+    }
+}
