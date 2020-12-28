7450e9bcaade90bfbd81088362b4c55c64e8d4f4
MaxRybetsky <stsdewar@ya.ru>
2020-12-28 21:07:15 +0300

    Spring MVC + Hibernate + AOP practise (Part 1)

---
+++ spring_course_mvc_hibernate_aop/src/main/java/com/maxrybetsky/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -0,0 +1,30 @@
+package com.maxrybetsky.spring.mvc_hibernate_aop.dao;
+
+import com.maxrybetsky.spring.mvc_hibernate_aop.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        Session session = sessionFactory.getCurrentSession();
+        List<Employee> employees = session.createQuery("from Employee", Employee.class)
+                .getResultList();
+        return employees;
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(employee);
+    }
+}
---
+++ spring_course_mvc_hibernate_aop/src/main/java/com/maxrybetsky/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.maxrybetsky.spring.mvc_hibernate_aop.service;
+
+import com.maxrybetsky.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+}
