53be65bc1b2e5b4b9180c89adbc9150672f7c61a
MaxRybetsky <stsdewar@ya.ru>
2021-01-08 19:32:06 +0300

    Add DAO Layer

--- spring_example_1/src/main/java/com/max/springmvc/dao/EmployeeDaoImpl.java
+++ spring_example_1/src/main/java/com/max/springmvc/dao/EmployeeDaoImpl.java
@@ -1,4 +1,43 @@
 package com.max.springmvc.dao;
 
-public class EmployeeDaoImpl {
+import com.max.springmvc.model.Employee;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("employeeDao")
+public class EmployeeDaoImpl extends AbstractDao<Integer, Employee> implements EmployeeDao {
+    @Override
+    public Employee findById(int id) {
+        return getByKey(id);
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        persist(employee);
+    }
+
+    @Override
+    public void deleteEmployeeBySsn(String ssn) {
+        Query query = getSession().createSQLQuery("delete from Employee where ssn=:ssn");
+        query.setString("ssn", ssn);
+        query.executeUpdate();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<Employee> findAllEmployees() {
+        Criteria criteria = createEntityCriteria();
+        return (List<Employee>) criteria.list();
+    }
+
+    @Override
+    public Employee findEmployeeBySsn(String ssn) {
+        Criteria criteria = createEntityCriteria();
+        criteria.add(Restrictions.eq("ssn", ssn));
+        return (Employee) criteria.uniqueResult();
+    }
 }
