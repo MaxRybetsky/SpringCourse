a49359230378ac1588b3eaad0b676e4f1d5e11e6
MaxRybetsky <stsdewar@ya.ru>
2021-01-13 12:24:38 +0300

    Spring REST Learning - get Employee by id in JSON

--- spring_rest/src/main/java/com/max/controller/MyRESTController.java
+++ spring_rest/src/main/java/com/max/controller/MyRESTController.java
@@ -4,6 +4,7 @@ import com.max.entity.Employee;
 import com.max.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,11 @@ public class MyRESTController {
 
     @GetMapping("/employees")
     public List<Employee> showAllEmployees() {
-        List<Employee> employees = service.getAllEmployees();
-        return employees;
+        return service.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        return service.getEmployee(id);
     }
 }
