4e517cb46915094fc1e4b9d9d8ccddc5589a18d2
MaxRybetsky <stsdewar@ya.ru>
2021-01-13 14:01:54 +0300

    Spring REST Learning (examples)

--- spring_rest/src/main/java/com/max/controller/MyRESTController.java
+++ spring_rest/src/main/java/com/max/controller/MyRESTController.java
@@ -1,12 +1,13 @@
 package com.max.controller;
 
 import com.max.entity.Employee;
+import com.max.exception_handling.EmployeeIncorrectData;
+import com.max.exception_handling.NoSuchEmployeeException;
 import com.max.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +24,34 @@ public class MyRESTController {
 
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable int id) {
-        return service.getEmployee(id);
+        Employee employee = service.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = "
+                    + id + " in Database");
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        service.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        service.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = service.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = "
+                    + id + " in Database");
+        }
+        service.deleteEmployee(id);
+        return "Employee with ID = " + id + " was deleted";
     }
 }
---
+++ spring_rest/src/main/java/com/max/exception_handling/NoSuchEmployeeException.java
@@ -0,0 +1,7 @@
+package com.max.exception_handling;
+
+public class NoSuchEmployeeException extends RuntimeException {
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+}
