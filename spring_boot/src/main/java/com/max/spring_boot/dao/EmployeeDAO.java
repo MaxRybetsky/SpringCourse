9f3d6376a48a18dd5a3ad56f70c8f7f6fd22c0be
MaxRybetsky <stsdewar@ya.ru>
2021-01-14 00:27:49 +0300

    REST API via Spring Boot

---
+++ spring_boot/src/main/java/com/max/spring_boot/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package com.max.spring_boot.dao;
+
+import com.max.spring_boot.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+}
