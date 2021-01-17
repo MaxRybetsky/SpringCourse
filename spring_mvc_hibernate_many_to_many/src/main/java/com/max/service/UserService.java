8ae35cf8228add0acf3bb06122cace3a53f71e5b
MaxRybetsky <stsdewar@ya.ru>
2021-01-17 21:26:05 +0300

    Spring 4 MVC+Hibernate Many-to-many JSP Example with annotation -  Create Service layer

--- spring_mvc_hibernate_many_to_many/src/main/java/com/max/service/UserService.java
+++ spring_mvc_hibernate_many_to_many/src/main/java/com/max/service/UserService.java
@@ -1,4 +1,19 @@
 package com.max.service;
 
+import com.max.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    User findById(int id);
+
+    User findBySSO(String sso);
+
+    void saveUser(User user);
+
+    void deleteUserBySSO(String sso);
+
+    List<User> findAllUsers();
+
+    boolean isUserSSOUnique(Integer id, String sso);
 }
--- spring_mvc_hibernate_many_to_many/src/main/java/com/max/service/UserServiceImpl.java
+++ spring_mvc_hibernate_many_to_many/src/main/java/com/max/service/UserServiceImpl.java
@@ -1,4 +1,49 @@
 package com.max.service;
 
-public class UserServiceImpl {
+import com.max.dao.UserDao;
+import com.max.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public User findById(int id) {
+        return userDao.findById(id).orElse(null);
+    }
+
+    @Override
+    public User findBySSO(String sso) {
+        return userDao.findBySSO(sso);
+    }
+
+    @Override
+    public void saveUser(User user) {
+        userDao.save(user);
+    }
+
+    @Override
+    public void deleteUserBySSO(String sso) {
+        userDao.deleteBySSO(sso);
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return userDao.findAll();
+    }
+
+    @Override
+    public boolean isUserSSOUnique(Integer id, String sso) {
+        User user = findBySSO(sso);
+        return (user == null || (user.getId().equals(id)));
+    }
 }
