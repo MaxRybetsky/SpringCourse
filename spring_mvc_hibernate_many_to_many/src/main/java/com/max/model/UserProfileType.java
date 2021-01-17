a7e2ed35354716255b99081dd6cb2a349c2f2385
MaxRybetsky <stsdewar@ya.ru>
2021-01-17 19:58:32 +0300

    Spring 4 MVC+Hibernate Many-to-many JSP Example with annotation - Prepare Model classes

--- spring_mvc_hibernate_many_to_many/src/main/java/com/max/model/UserProfileType.java
+++ spring_mvc_hibernate_many_to_many/src/main/java/com/max/model/UserProfileType.java
@@ -1,4 +1,17 @@
 package com.max.model;
 
-public class UserProfileType {
+public enum UserProfileType {
+    USER("USER"),
+    DBA("DBA"),
+    ADMIN("ADMIN");
+
+    private String userProfileType;
+
+    private UserProfileType(String userTypeProfile) {
+        this.userProfileType = userTypeProfile;
+    }
+
+    public String getUserProfileType() {
+        return userProfileType;
+    }
 }
