3091942e04b6e2838d97c5b8bb88d0ae49a08850
MaxRybetsky <stsdewar@ya.ru>
2021-01-18 13:20:28 +0300

    Spring 4 MVC+Hibernate Many-to-many JSP Example with annotation - Create Converter

--- spring_mvc_hibernate_many_to_many/src/main/java/com/max/converter/RoleToUserProfileConverter.java
+++ spring_mvc_hibernate_many_to_many/src/main/java/com/max/converter/RoleToUserProfileConverter.java
@@ -1,4 +1,24 @@
 package com.max.converter;
 
-public class RoleToUserProfileConverter {
+import com.max.model.UserProfile;
+import com.max.service.UserProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+
+public class RoleToUserProfileConverter implements Converter <Object, UserProfile>{
+
+    private UserProfileService userProfileService;
+
+    @Autowired
+    public RoleToUserProfileConverter(UserProfileService userProfileService) {
+        this.userProfileService = userProfileService;
+    }
+
+    @Override
+    public UserProfile convert(Object o) {
+        Integer id = Integer.parseInt((String) o);
+        UserProfile profile = userProfileService.findById(id);
+        System.out.println("Profile: " + profile);
+        return profile;
+    }
 }
